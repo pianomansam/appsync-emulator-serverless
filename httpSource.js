3e9ace7e5f04ec56022110d9cd2bbfc13100e85f
Samuel Oltz <soltz@rapiddg.com>
2018-11-26 15:55:29 -0500

    additional conditions to not add queryPath if query doesnt have keys

--- httpSource.js
+++ httpSource.js
@@ -1,12 +1,13 @@
-const BackwardCompatibleURLSearchParams = require('url').URLSearchParams;
+const { URLSearchParams } = require('url');
 const fetch = require('node-fetch');
 
 const httpSource = async (endpoint, { resourcePath, method, params }) => {
   const { query } = params || {};
   const queryPath =
-    query === undefined
+    query === undefined ||
+    (typeof query === 'object' && Object.keys(query).length === 0)
       ? ''
-      : `?${new BackwardCompatibleURLSearchParams(query)}`;
+      : `?${new URLSearchParams(query)}`;
 
   const response = await fetch(endpoint + resourcePath + queryPath, {
     ...params,
